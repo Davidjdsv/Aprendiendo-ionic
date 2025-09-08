0aa9a9276067609a9f817f4a620b2b90d9189b1b
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-09-08 17:20:09 -0500

    feat: Creando componentes personalizados 'home' y 'customers' aprendiendo Ionic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,4 +11,25 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'inicio',
+    loadComponent: () => import('./inicio/inicio.page').then( m => m.InicioPage)
+  },
+  {
+    path: 'products',
+    loadComponent: () => import('./products/products.page').then( m => m.ProductsPage)
+  },
+  {
+    path: 'customers',
+    loadComponent: () => import('./customers/customers.page').then( m => m.CustomersPage)
+  },
+  {
+    path: 'products',
+    loadComponent: () => import('./products/products.page').then( m => m.ProductsPage)
+  },
+
 ];
