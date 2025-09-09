d6172a4e7a8821f36b35125bd15447be21134622
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-09-09 10:43:00 -0500

    feat: Eliminando rutas innecesarias del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,25 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
+    redirectTo: 'home',
     pathMatch: 'full',
-  },
-  {
-    path: 'folder/:id',
-    loadComponent: () =>
-      import('./folder/folder.page').then((m) => m.FolderPage),
   },
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then( m => m.HomePage)
-  },
-  {
-    path: 'inicio',
-    loadComponent: () => import('./inicio/inicio.page').then( m => m.InicioPage)
-  },
-  {
-    path: 'products',
-    loadComponent: () => import('./products/products.page').then( m => m.ProductsPage)
   },
   {
     path: 'customers',
