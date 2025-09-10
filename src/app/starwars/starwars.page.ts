3938c3a3e2856252d27d22a38b7a224bc76dda85
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-09-10 17:49:10 -0500

    feat:Añadiendo barra de búsqueda en la página de starwars

--- src/app/starwars/starwars.page.ts
+++ src/app/starwars/starwars.page.ts
@@ -2,7 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonAvatar, IonItemSliding } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonAvatar, IonItemSliding, IonSearchbar } from '@ionic/angular/standalone';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { map } from 'rxjs';
@@ -13,7 +13,7 @@ import { map } from 'rxjs';
   templateUrl: './starwars.page.html',
   styleUrls: ['./starwars.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonLabel, IonAvatar, IonItemSliding]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonLabel, IonAvatar, IonItemSliding, IonSearchbar]
 })
 export class StarwarsPage implements OnInit {
   
@@ -21,15 +21,18 @@ export class StarwarsPage implements OnInit {
 
   starWars: any[] = []; // Array para almacenar los datos de la API
   permisos!: boolean;
-
+  searchText: any;
+  
+  
   ngOnInit() {
     this.permisos = true;
     this.getStarWars().subscribe(res => {
       console.log(res);
       this.starWars = res; // Aloja los datos en la variable starWars en el array para luego ser recorrido
+      this.searchText = this.starWars; // Almacena los datos en la variable searchText para luego ser filtrados
     });
   }
-
+  
   getStarWars() {
     //Obtiene la ruta a consultar a datos o a apis
     //pipe funciona para transformar los datos
@@ -40,6 +43,17 @@ export class StarwarsPage implements OnInit {
         return res.data;
       })
     );
+  }
+
+  foundStarWars(event: any) {
+    const text = event.target.value;
+    if(text && text.trim() != ""){
+      this.searchText = this.searchText.filter((user: any) => {
+        return user.name.toLowerCase().indexOf(text.toLowerCase()) > -1;
+      })
+    } else {
+      this.searchText = [...this.starWars];
+    }
   }
 
 }
