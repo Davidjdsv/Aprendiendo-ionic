1803fbd6c6f37f9fc6ae5025b9e6a92780c7e765
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-09-10 10:14:20 -0500

    feat:Mostrando los datos del json en el componente starwars

--- src/app/starwars/starwars.page.ts
+++ src/app/starwars/starwars.page.ts
@@ -1,25 +1,30 @@
+// importar lo necesario
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonAvatar, IonItemSliding, IonItemOptions, IonItemOption, IonIcon } from '@ionic/angular/standalone';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { map } from 'rxjs';
 
+//Usar los imports en el componente
 @Component({
   selector: 'app-starwars',
   templateUrl: './starwars.page.html',
   styleUrls: ['./starwars.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonLabel, IonAvatar, IonItemSliding, IonItemOptions, IonItemOption, IonIcon]
 })
 export class StarwarsPage implements OnInit {
   
   constructor(private router: Router, private http: HttpClient) { }
 
+  starWars: any[] = []; // Array para almacenar los datos de la API
+
   ngOnInit() {
     this.getStarWars().subscribe(res => {
       console.log(res);
+      this.starWars = res; // Aloja los datos en la variable starWars en el array para luego ser recorrido
     });
   }
 
