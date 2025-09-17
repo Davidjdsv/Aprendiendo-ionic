ebd974e86f2bf81fd905a943d63ab363808e8dd5
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-09-17 10:18:31 -0500

    feat(components): implement parent-child data binding in Angular
    Add input binding to child component to display dynamic title from parent
    Include input field in parent to update title in real-time
    Update component imports and standalone declarations

--- src/app/components/father/father.component.ts
+++ src/app/components/father/father.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ChildComponent } from '../child/child.component';
+import { IonInput } from '@ionic/angular/standalone'
 
 @Component({
   selector: 'app-father',
   templateUrl: './father.component.html',
   styleUrls: ['./father.component.scss'],
+  imports: [ChildComponent, IonInput, FormsModule]
 })
 export class FatherComponent  implements OnInit {
+  title: string = "Hola, estoy aprendiendo Angular" // Atributo que se envia al mismo padre en html
 
   constructor() { }
 
