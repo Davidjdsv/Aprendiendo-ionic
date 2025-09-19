5dff502feef67ae82992008d94cdd8e883b4a932
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-09-19 17:57:29 -0500

    feat(rick-and-morty): improve character display with cards and grid layout
    - Replace simple list with responsive grid of cards showing character details
    - Use RxJS pipe and map operators for cleaner data transformation
    - Remove unused code and simplify component logic

--- src/app/rick-and-morty/rick-and-morty.page.ts
+++ src/app/rick-and-morty/rick-and-morty.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { IonContent,IonGrid, IonCol, IonRow, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
 import { SharedMenuComponent } from '../components/shared-menu/shared-menu.component';
 import { RickAndMorty } from '../services/rickAndMorty/rick-and-morty';
 import { RickAndMortyMdl } from '../modelos/rickAndMorty/rick-and-morty';
@@ -12,20 +12,35 @@ import { map } from 'rxjs';
   templateUrl: './rick-and-morty.page.html',
   styleUrls: ['./rick-and-morty.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, SharedMenuComponent, IonList, IonItem, IonLabel],
+  imports: [IonContent, CommonModule, FormsModule, SharedMenuComponent,IonGrid, IonCol, IonRow, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle]
 })
 export class RickAndMortyPage implements OnInit {
   rmCharacters: RickAndMortyMdl[] = [];
-  rmpj: any[] = [];
-
   constructor(private rmResponse: RickAndMorty) {}
 
-  async ngOnInit() {
-    this.rmResponse.getRmCharactersAPI().subscribe({
-    next:(data)=>{
-      let datos = data.results;
+  // ngOnInit() {
+  //   this.rmResponse.getRmCharactersAPI().subscribe({
+  //   next:(data)=>{
+  //     let datos = data.results;
 
-      this.rmpj = datos.map((d:any) => (
+  //     this.rmCharacters = datos.map((d:any) => (
+  //     {
+  //       id: d.id,
+  //       name: d.name,
+  //       status: d.status,
+  //       species: d.species,
+  //       image: d.image
+  //     }
+  //     ))
+  //   },
+  //   error:(e)=>{
+  //     console.error(e)
+  //   }
+  //   })
+  // }
+    ngOnInit() {
+    this.rmResponse.getRmCharactersAPI().pipe(
+      map(res => res.results.map((d:any) => (
       {
         id: d.id,
         name: d.name,
@@ -33,12 +48,15 @@ export class RickAndMortyPage implements OnInit {
         species: d.species,
         image: d.image
       }
-      ))
+      )))
+    ).subscribe({
+    next:(data)=>{
+      console.log("data: ", data);
+      this.rmCharacters = data
     },
     error:(e)=>{
       console.error(e)
     }
     })
-
   }
 }
